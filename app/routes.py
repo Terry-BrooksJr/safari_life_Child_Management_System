1d310068b9c20a405e44185dfd28602f7c6c5394
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2022-03-05 18:35:47 -0600

    Adding Custom 404

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from flask import Flask, redirect, url_for, render_template, request, abort
+
+
+from app import app
+
+@app.route('/')
+@app.route('/home')
+@app.route('/index')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/dashboard', methods=['POST','GET'])
+def dashboard():
+    return redirect('dashboard.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html',404)``
