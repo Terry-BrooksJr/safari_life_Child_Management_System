29529e1d71ea99d6ecfdcf270d0d49d74bc73edb
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2022-03-05 18:14:36 -0600

    Framewortk up

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask,redirect,url_for,render_template,request
 
-app=Flask(__name__)
-@app.route('/')
-@app.route('/home')
-@app.route('/index')
-def home():
-    return "<h1>Hello Earth!!<H1>"
+app = Flask(__name__)
+
+from app import routes 
+
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
